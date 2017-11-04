66a82ab27a691743b7031928313c6c940114e227
hhrose1307 <33056191+hhrose1307@users.noreply.github.com>
2017-11-04 07:50:24 +0700

    All

---
+++ TravelWeb/Controllers/MapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelWeb.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
